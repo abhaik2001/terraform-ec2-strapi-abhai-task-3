c7e243cc6049982dbcb74f44b10bdfa8f428cb85
abhai k <158554525+abhaik2001@users.noreply.github.com>
2026-02-04 17:52:49 +0530

    Task-3: Provision EC2 with Terraform modules and deploy Strapi

---
+++ modules/ec2/main.tf
@@ -0,0 +1,51 @@
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "this" {
+  key_name   = var.key_name
+  public_key = tls_private_key.this.public_key_openssh
+}
+
+resource "local_file" "pem" {
+  content         = tls_private_key.this.private_key_pem
+  filename        = "${var.key_name}.pem"
+  file_permission = "0400"
+}
+
+resource "aws_security_group" "this" {
+  name = "strapi-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "this" {
+  ami                    = "ami-0f5ee92e2d63afc18" # Ubuntu 22.04 (Mumbai)
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.this.key_name
+  vpc_security_group_ids = [aws_security_group.this.id]
+
+  tags = {
+    Name = "Strapi-EC2-Abhai"
+  }
+}
